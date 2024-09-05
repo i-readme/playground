7314933ab931375c34ed7db18bc36931aebeb1b9
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-05 18:27:34 +0200

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        lightBackground: '#ffffff',
+        lightText: '#282c34',
+        darkBackground: '#282c34',
+        darkText: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+};
