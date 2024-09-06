113212927af0777b706532aed9b401d5bbfbb7f6
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-06 14:51:42 +0200

    clean up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import NavBar from '@components/navigation/NavBar';
 import { useTheme } from '@utils/ThemeContext';
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App: React.FC = () => {
   const { theme } = useTheme();
+  const [markdown, setMarkdown] = useState('hello');
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const toggleHandler = () => setToggleMenu((prevState) => !prevState);
+
   const resetHandler = () => {
     console.log('click');
+    setMarkdown('');
   };
+  //TODO: add sidebar menu
+  console.log(toggleMenu);
+
   return (
     <>
-      <NavBar reset={resetHandler} />
+      <NavBar reset={resetHandler} download={markdown} toggle={toggleHandler} />
       <main className={theme}>
         <div className="flex min-h-screen">
           <Routes>
@@ -22,4 +31,5 @@ const App: React.FC = () => {
     </>
   );
 };
+
 export default App;
