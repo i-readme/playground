59c34730f7c37bb62969a391cb0c83584e544ebc
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-06 13:51:38 +0200

    clean home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,7 @@
-import heart from '../assets/icons/heart.svg';
-import ThemeSwitch from '../components/controllers/ThemeSwitch';
-
 const Home: React.FC = () => {
   return (
-    <div>
-      <p>Hello , welcome home</p>
-
-      <div className="App">
-        <ThemeSwitch />
-
-        <header className="App-header">
-          <img src={heart} alt="Logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          Hello
-        </header>
-      </div>
+    <div className="flex items-center">
+      <p className="space-y-7">Hello , welcome home</p>
     </div>
   );
 };
