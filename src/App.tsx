8b42acaccda3e2dce89cf82bc57ad2ecca23ea5f
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-06 13:51:07 +0200

    cleanup useless items

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
+import NavBar from '@components/navigation/NavBar';
+import { useTheme } from '@utils/ThemeContext';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import { useTheme } from '@utils/ThemeContext';
 
 const App: React.FC = () => {
   const { theme } = useTheme();
+  const resetHandler = () => {
+    console.log('click');
+  };
   return (
-    <main className={theme}>
-      <div className="flex min-h-screen flex-row justify-between">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </main>
+    <>
+      <NavBar reset={resetHandler} />
+      <main className={theme}>
+        <div className="flex min-h-screen">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </main>
+    </>
   );
 };
 export default App;
